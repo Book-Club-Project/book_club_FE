617503dc4366deb33e208dfe2514c94671dc3b4c
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-27 12:07:43 -0500

    Initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def create
+    conn = Faraday.new(url: 'http://localhost:3000/')
+
+    auth_hash = request.env['omniauth.auth']
+    email = auth_hash['info']['email']
+
+    session[:access_token] = auth_hash['credentials']['token']
+
+    response = conn.post('/api/v1/users') do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.body = JSON.generate(user: { username: email, email: email })
+    end
+    redirect_to '/'
+  end
+end
