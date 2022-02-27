2bbffd2e14522e7c98db818c4c4fe2b26a4fbff9
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-27 12:03:54 -0500

    Modify book_info method

--- app/services/book_service.rb
+++ app/services/book_service.rb
@@ -1,6 +1,6 @@
 class BookService
   def self.conn
-    conn = Faraday.new(url: "http://localhost:3000")
+    conn = Faraday.new(url: "http://localhost:3000/")
   end
 
   def self.parsed_response(url)
@@ -13,8 +13,8 @@ class BookService
   end
 
   def self.book_info(book_id)
-    response = parsed_response("/api/v1/books/#{book_id}")
-
+    response = conn.get("api/v1/books/#{book_id}")
+    JSON.parse(response.body, symbolize_names: true)
   end
 
 end
