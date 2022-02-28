005de5db9ffb1cf2ab20aa1bdfe3230ce4f16b6b
Brad Breiten <jonathan@breiten.us>
2022-02-28 14:15:35 -0700

    Test: add tests for landing page content

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,15 +1,97 @@
 require 'rails_helper'
 
-RSpec.describe  'Book Club Landing/Welcome page' do
-  it 'shows the landing page' do
-    visit ('/')
+RSpec.describe 'Book Club Landing/Welcome page', type: :feature do
+  #As an un-authenticated visitor
+  context 'As a visitor to the landing page' do
+    before(:each) { visit '/' }
 
-    expect(page).to have_content('Book Club')
+    scenario 'I am on the root directory' do
+      expect(page).to have_current_path('/')
+    end
 
-    # This will be filled with Brad's Quote API
-    expect(page).to have_content('To Be or Not To Be')
+    scenario 'I see a welcome to app message' do
+      expect(page).to have_content('Welcome to Book Club')
+    end
 
-    click_link 'Discover Books'
-    expect(current_path).to eq('/discover')
+    context 'I see links' do
+      scenario 'home link' do
+        expect(page).to have_link('Home', href: '/')
+        click_link 'Home'
+        expect(page).to have_current_path('/')
+      end
+
+      scenario 'discover books link' do
+        expect(page).to have_link('Discover Books', href: '/books/discover')
+        click_link 'Discover Books'
+        expect(page).to have_current_path('/books/discover')
+      end
+
+      scenario 'login and register links' do
+        expect(page).to have_link('Login', href: '/login')
+        expect(page).to have_link('Register', href: '/register')
+      end
+    end
+
+    scenario 'I see a random quote' do
+      expect(page).to have_css('#random-quote')
+    end
+
+    scenario 'I do not see links for logged in users' do
+      expect(page).to_not have_link('Logout')
+      expect(page).to_not have_link('My Page')
+      expect(page).to_not have_css('#dashboard-link')
+    end
+  end
+
+  #As an authenticated visitor
+  xcontext 'As a logged in user visiting the landing page' do
+    before(:each) do
+      visit '/'
+      #authenticate user before runing tests
+    end
+
+    scenario 'I am on the root directory' do
+      expect(page).to have_current_path('/')
+    end
+
+    scenario 'I see a welcome to app message' do
+      expect(page).to have_content('Welcome to Book Club')
+    end
+
+    context 'I see links' do
+      scenario 'home link' do
+        expect(page).to have_link('Home', href: '/')
+        click_link 'Home'
+        expect(page).to have_current_path('/')
+      end
+
+      scenario 'discover books link' do
+        expect(page).to have_link('Discover Books', href: '/books/discover')
+        click_link 'Discover Books'
+        expect(page).to have_current_path('/books/discover')
+      end
+
+      scenario 'logout link' do
+        expect(page).to have_link('Logout', href: '/logout')
+      end
+
+      scenario 'link to my page' do
+        expect(page).to have_link('My Page')
+        expect(page).to have_css('#dashboard-link')
+      end
+
+      scenario 'I do not see links for unauthenticated visitors' do
+        expect(page).to_not have_link('Login')
+        expect(page).to_not have_link('Register')
+      end
+    end
+
+    scenario 'I see a random quote' do
+      expect(page).to have_css('#random-quote')
+
+      within "#random-quote" do
+        expect(page).to have_content("Quote of the day!")
+      end
+    end
   end
 end
