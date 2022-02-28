b08568703d07471c724483ed27bde6460be703d6
Brad Breiten <jonathan@breiten.us>
2022-02-28 10:07:39 -0700

    Add routes for login, register, and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
-  get '/books/:id', to: "books#show"
+  root  'welcome#index'
+  get   '/books/:id', to: "books#show"
   # get '/discover', to: 'books#index'
-  get '/auth/google_oauth2/callback', to: 'users#create'
+  get   '/register', to: 'users#new'
+
+  get   '/login', to: 'sessions#new'
+  post  '/login', to: 'sessions#create'
+  get   '/logout', to: 'sessions#delete'
+
+  get   '/auth/google_oauth2/callback', to: 'users#create'
 end
