3e2043cd9765e4e2365ecfaca24a7e9abc6b4066
Brad Breiten <jonathan@breiten.us>
2022-02-28 14:58:44 -0700

    Fix indentation

--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -1,6 +1,6 @@
 class BookFacade
 
-def self.book_show(book_id)
-  Book.new(BookService.book_info(book_id))
-end
+  def self.book_show(book_id)
+    Book.new(BookService.book_info(book_id))
+  end
 end
