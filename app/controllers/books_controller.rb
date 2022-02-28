02733d7456ef0d04791ddab0161281c1544b6220
Mallory Vining <mallorygallery12@gmail.com>
2022-02-28 13:15:59 -0700

    Oops forgot this

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,21 @@
 class BooksController < ApplicationController
+  def index
+    if params[:search]
+      # binding.pry
+      @books = BookFacade.find_book(params[:search])
+      redirect_to '/discover'
+    # else
+    #   redirect_to '/discover'
+    #   flash[:error] = "We failed you or this book does not exist"
+    end
+  end
 
   def show
     @book = BookFacade.book_show(params[:id])
+  end
+
+  private
+  def book_params
+    params.require(:books).permit(:title, :author, :summary, :rating, :genre)
   end
 end
--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -1,6 +1,12 @@
 class BookFacade
+  def self.find_book(query)
+    books = BookService.search_books(query)[:data]
+    books.map do |data|
+      Book.new(data)
+    end
+  end
 
-def self.book_show(book_id)
-  Book.new(BookService.book_info(book_id))
-end
+  def self.book_show(book_id)
+    Book.new(BookService.book_info(book_id))
+  end
 end
