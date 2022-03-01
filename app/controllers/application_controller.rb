8dd9410e49480af3c431a0c5a0a43f61419b11d5
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-28 20:30:21 -0500

    Add current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  def current_user
+    # require "pry"; binding.pry
+    # @new_user if session[:user_id]
+
+  end
 end
