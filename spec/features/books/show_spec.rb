d6a0234b77955d55123d06a0e76b22c7c7da1480
Eric Chon <echon92@gmail.com>
2022-03-02 11:08:11 -0700

    updated test to check for image

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -8,5 +8,9 @@ RSpec.describe 'Books Show Page' do
     expect(page).to have_button('Create Book Club')
     expect(page).to have_content('Jane Austen')
     expect(page).to have_content('Fiction / Classics')
+    expect(page).to have_css("#book-cover")
+    within "#book-cover" do
+      expect(page).to have_css("#cover-image")
+    end
   end
 end
