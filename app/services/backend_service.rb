90eac2548362b0a7c3df9ba2b7aa25a493c44c0d
Mallory Vining <mallorygallery12@gmail.com>
2022-03-02 20:46:33 -0700

    add needed code to ignore warnings and run local host

--- app/services/backend_service.rb
+++ app/services/backend_service.rb
@@ -1,6 +1,7 @@
 class BackendService
   def self.conn
-    conn = Faraday.new(url: "https://boiling-woodland-43020.herokuapp.com/")
+     conn = Faraday.new(url: "http://localhost:3000/")
+    # conn = Faraday.new(url: "https://boiling-woodland-43020.herokuapp.com/")
   end
 
   def self.parsed_response(url)
