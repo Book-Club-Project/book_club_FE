ddaaa5b9ce945a039166f0642a0c713a996d502c
Brad Breiten <jonathan@breiten.us>
2022-03-02 17:28:30 -0700

    Progress towards authenticating a user in tests

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,10 @@
 class WelcomeController < ApplicationController
 
   before_action :quote, only: [:index]
+  before_action :current_user
 
   def index
+    require "pry"; binding.pry
   end
 
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -90,3 +90,19 @@ VCR.configure do |config|
   # config.filter_sensitive_data('<api_key>') { ENV[''] }
   config.configure_rspec_metadata!
 end
+
+Capybara.default_host = 'http://example.org'
+OmniAuth.config.test_mode = true
+OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+    :provider => "google_oauth2",
+    :uid => "12345",
+    :info => {
+      :name => "Raccoon22",
+      :email => "happy22@example.com"
+    },
+    :credentials => {
+      :token => "token",
+      :refresh_token => "refresh token"
+    }
+  }
+)
