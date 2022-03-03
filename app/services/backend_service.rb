10b14d9f1fe26f76ced198f0d249e03ad5c19ea9
Brad Breiten <jonathan@breiten.us>
2022-03-03 14:16:24 -0700

    Add clarifying comment to code

--- app/services/backend_service.rb
+++ app/services/backend_service.rb
@@ -1,9 +1,10 @@
 class BackendService
+
   def self.conn
+    conn = Faraday.new(url: "http://localhost:3000/")
 
-     conn = Faraday.new(url: "http://localhost:3000/")
+    # Heroku app connection code
     # conn = Faraday.new(url: "https://boiling-woodland-43020.herokuapp.com/")
-
   end
 
   def self.parsed_response(url)
