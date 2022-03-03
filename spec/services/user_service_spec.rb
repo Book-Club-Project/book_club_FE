4913dd5cc848b563c33efd2b5d76e77cb07c53b9
Brad Breiten <jonathan@breiten.us>
2022-03-03 14:27:31 -0700

    Test: add test for get_all_users method

--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -2,19 +2,40 @@ require 'rails_helper'
 
 RSpec.describe UserService do
   describe 'class methods' do
-    it 'returns a user data', :vcr do
-      response = UserService.get_user(1)
-      expect(response).to be_a Hash
+    describe 'get_user' do
+      it 'returns a user data', :vcr do
+        response = UserService.get_user(1)
 
+        expect(response).to be_a Hash
 
-      expect(response).to have_key :id
-      expect(response[:id]).to be_a String
+        expect(response).to have_key :id
+        expect(response[:id]).to be_a String
 
-      expect(response[:attributes]).to have_key :username
-      expect(response[:attributes][:username]).to be_a String
+        expect(response[:attributes]).to have_key :username
+        expect(response[:attributes][:username]).to be_a String
 
-      expect(response[:attributes]).to have_key :email
-      expect(response[:attributes][:email]).to be_a String
+        expect(response[:attributes]).to have_key :email
+        expect(response[:attributes][:email]).to be_a String
+      end
+    end
+
+    describe 'get_all_users' do
+      it 'returns data for all users', :vcr do
+        response = UserService.get_all_users
+
+        expect(response).to be_a Array
+
+        response.each do |user|
+          expect(user).to have_key :id
+          expect(user[:id]).to be_a String
+
+          expect(user[:attributes]).to have_key :username
+          expect(user[:attributes][:username]).to be_a String
+
+          expect(user[:attributes]).to have_key :email
+          expect(user[:attributes][:email]).to be_a String
+        end
+      end
     end
   end
 end
