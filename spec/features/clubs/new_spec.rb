188089a5f60a4801f8987d43a2e38281d8e91fa1
Brad Breiten <jonathan@breiten.us>
2022-03-03 22:14:03 -0700

    Test: new club page MVP covered

--- spec/features/clubs/new_spec.rb
+++ spec/features/clubs/new_spec.rb
@@ -1,39 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe "New club page" do
-  before(:each) do
-  user = User.new({ id: '1', attributes: { name: "Raccoon22", email: "happy22@example.com" } })
+  context 'As an authenticated user', :vcr do
+    let!(:user) { User.new({ id: '1', attributes: { name: "Raccoon22", email: "happy22@example.com" } }) }
 
-  OmniAuth.config.test_mode = true
+    before(:each) do
+      OmniAuth.config.test_mode = true
 
-  #we are getting CSRF get vs. post errors, and have chosen to ignore them
-  OmniAuth.config.silence_get_warning = true
+      # Silence CSRF attack warnings
+      OmniAuth.config.silence_get_warning = true
 
-  OmniAuth.config.add_mock(:google, {:uid => '12345'})
-  Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+      # Mock OAuth user authentication
+      OmniAuth.config.add_mock(:google, {:uid => '12345'})
+      Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
 
-  allow_any_instance_of(ApplicationController)
-      .to receive(:current_user).and_return(user)
+      # Set current user
+      allow_any_instance_of(ApplicationController)
+          .to receive(:current_user).and_return(user)
+
       visit "/auth/google_oauth2"
-  end
+    end
 
-  it "check for a create new club button", :vcr do
-    visit("/books/gwgON2IwgDUC")
+    context 'Content displays for authenticated user' do
+      before(:each) do
+        visit '/books/gwgON2IwgDUC'
+        click_button "Create Book Club"
+      end
 
-    click_button "Create Book Club"
+      scenario 'I am on the create club page' do
+        expect(current_path).to eq("/clubs/new")
+      end
 
-    expect(current_path).to eq("/clubs/new")
-  end
+      # Navbar behavior is tested in welcome/index_spec.rb
+      scenario 'I see the nav bar links for authenticated user' do
+        expect(page).to have_link('Home', href: '/')
+        expect(page).to have_link('Discover Books', href: '/discover')
 
-  it "checks to see if we can create a new club", :vcr do
-    visit("/books/gwgON2IwgDUC")
+        expect(page).to_not have_link('Login', href: '/login')
+        expect(page).to_not have_link('Register', href: '/register')
 
-    click_button "Create Book Club"
+        expect(page).to have_link('Logout')
+        expect(page).to have_link('My Dashboard')
+      end
 
-    fill_in :name, with: "Hello123"
+      # Skipped to not blow out API rate limit
+      xscenario 'I see a random quote' do
+        expect(page).to have_css('#random-quote')
+      end
 
-    click_button "Create"
+      scenario 'I see a form to create a book club' do
+        expect(page).to have_field(:name)
+        expect(page).to have_content('Select Users to Add')
+        expect(page).to have_css('#select-user-1')
+        expect(page).to have_button('Create Book Club')
+      end
 
-    expect(current_path).to match('/clubs/')
+      scenario "I can fill in information and create a new club" do
+        within '#form' do
+          fill_in :name, with: "Hello123"
+          click_button "Create Book Club"
+        end
+
+        expect(current_path).to match('/clubs/')
+      end
+    end
   end
 end
