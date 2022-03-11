9558f9001fc820428109198ec4d02e5285fa39ff
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-03-10 22:24:35 -0500

    Run rubocop

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,5 +1,4 @@
 class ClubsController < ApplicationController
-
   def show
     club_host = current_user
     club_id = params[:id]
@@ -20,12 +19,14 @@ class ClubsController < ApplicationController
       params[:user_ids] = []
       params[:user_ids].push(current_user.id)
       @book_id = params[:book_id]
-      @club = ClubFacade.create_club(name: params[:name], book_id: params[:book_id], host_id: current_user.id, params: params[:user_ids])
+      @club = ClubFacade.create_club(name: params[:name], book_id: params[:book_id], host_id: current_user.id,
+                                     params: params[:user_ids])
       redirect_to "/clubs/#{@club.id}"
     else
       @book_id = params[:book_id]
       params[:user_ids].push(current_user.id)
-      @club = ClubFacade.create_club(name: params[:name], book_id: params[:book_id], host_id: current_user.id, params: params[:user_ids])
+      @club = ClubFacade.create_club(name: params[:name], book_id: params[:book_id], host_id: current_user.id,
+                                     params: params[:user_ids])
       redirect_to "/clubs/#{@club.id}"
     end
   end
