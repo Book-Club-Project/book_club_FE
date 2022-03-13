e8350d67dac55afa1d4c438a6a13afdba8a75b60
Brad Breiten <jonathan@breiten.us>
2022-03-13 17:44:22 -0600

    Update landing page CSS Merge landing page and discover page Add new nav bar partial

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,9 +2,10 @@ class BooksController < ApplicationController
   def index
     if params[:search].present? && BookFacade.find_book(params[:search]).nil?
       flash[:error] = "We failed you or this book does not exist"
+
     elsif params[:search].present?
       @books = BookFacade.find_book(params[:search])
-      #need a way to refresh page as flash error is persisting through. 
+      #need a way to refresh page as flash error is persisting through.
     end
   end
 
